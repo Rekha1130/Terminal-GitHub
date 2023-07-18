aa1f9dea3c2a70d5c640cee38df29b565640b56c
Rekha1130 <erina.rekha@gmail.com>
2023-07-17 18:51:08 -0700

    Initial Commit

---
+++ Terminal/TerminalApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalApp.swift
+//  Terminal
+//
+//  Created by Rekha Aldas on 17/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TerminalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
